04f76cc0495948d4704adf632949449d928f3f29
Steffengj <103410399+SteffenGJ@users.noreply.github.com>
2023-09-13 17:33:30 +0200

    fixing images

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/images", express.static("public"));
 app.use("/api/users", userRouter);
 app.use("/api/content/:id", hasAccessToContent, contentRoute);
 app.use("/api/admin", adminRouter);
